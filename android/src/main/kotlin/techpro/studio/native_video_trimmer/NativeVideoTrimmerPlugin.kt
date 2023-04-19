6b9166b994f879e0f0f0c45588d385d8584d9e61
alex <alex@techpro.studio>
2023-04-19 19:24:20 +0300

    updates due to update of android package

--- android/src/main/kotlin/techpro/studio/native_video_trimmer/NativeVideoTrimmerPlugin.kt
+++ android/src/main/kotlin/techpro/studio/native_video_trimmer/NativeVideoTrimmerPlugin.kt
@@ -1,10 +1,16 @@
 package techpro.studio.native_video_trimmer
 
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
-import android.net.Uri
-import android.util.Log
-import androidx.annotation.NonNull
+import android.content.pm.PackageManager
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
 import com.gowtham.library.utils.TrimVideo
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
@@ -13,7 +19,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry
 
 
 object TrimVideoErrors {
@@ -21,19 +26,26 @@ object TrimVideoErrors {
 }
 
 /** NativeVideoTrimmerPlugin */
-class NativeVideoTrimmerPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
+class NativeVideoTrimmerPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   private lateinit var channel : MethodChannel
-  private lateinit var activityPluginBinding: ActivityPluginBinding
+  private var activity: FragmentActivity? = null
   private lateinit var result: Result
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  private lateinit var startForResult: ActivityResultLauncher<Intent>
+  
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "native_video_trimmer")
     channel.setMethodCallHandler(this)
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "run") {
       this.result = result
+
       trimVideo(call.argument("path")!!)
     } else {
       result.notImplemented()
@@ -41,41 +53,35 @@ class NativeVideoTrimmerPlugin: FlutterPlugin, MethodCallHandler, ActivityAware,
   }
 
   private fun trimVideo(path: String) {
-    TrimVideo.activity("file://$path").setMinDuration(1).setAccurateCut(true).start(activityPluginBinding.activity)
-  }
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
-    if (requestCode == TrimVideo.VIDEO_TRIMMER_REQ_CODE && data != null) {
-      try {
-        result.success(TrimVideo.getTrimmedVideoPath(data))
-      } catch (e: Exception) {
-        result.error(TrimVideoErrors.trimErrorCode, e.message, null);
-      }
-      return true;
-    }
-    return false
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+    TrimVideo.activity("file://$path").setMinDuration(1).setAccurateCut(true).start(activity, startForResult);
   }
 
   override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    this.activityPluginBinding = binding
-    binding.addActivityResultListener(this)
+    activity = binding.activity as? FragmentActivity
+    startForResult = activity!!.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+      result: ActivityResult ->
+      if (result.resultCode == Activity.RESULT_OK &&
+              result.data != null) {
+        try {
+          this.result.success(TrimVideo.getTrimmedVideoPath(result.data));
+        } catch (e: Exception) {
+          this.result.error(TrimVideoErrors.trimErrorCode, e.message, null);
+        }
+      }
+    }
+    print("Attached")
   }
 
   override fun onDetachedFromActivityForConfigChanges() {
-
+    activity = null
   }
 
   override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    this.activityPluginBinding.removeActivityResultListener(this)
-    this.activityPluginBinding = binding
-    binding.addActivityResultListener(this)
+    activity = binding.activity as? FragmentActivity
   }
 
   override fun onDetachedFromActivity() {
-    activityPluginBinding.removeActivityResultListener(this)
+    activity = null
   }
+
 }
